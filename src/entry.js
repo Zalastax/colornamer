1e5654b40db8bd6ba80236b7c1672b4baf232f6c
Pierre Krafft <kpierre@outlook.com>
2017-04-04 16:42:34 +0200

    Add colorpicker and improved parsing

--- src/entry.js
+++ src/entry.js
@@ -1,25 +1,47 @@
 require('file-loader?name=[name].[ext]!./index.html')
 var util = require('./util')
+var ColorPicker = require('./FlexiColorPicker/colorpicker.js')
 
 var colorlists = {
     xkcd: require('./xkcd'),
     official: require('./official')
 }
 
-var myFormEl = document.getElementById('myForm')
 var colorlistEl = document.getElementById('colorlist')
 var outEl = document.getElementById('out')
 var whatEl = document.getElementById('what')
+var sliderEl = document.getElementById('slider-indicator')
+var pickerEl = document.getElementById('picker-indicator')
 
-myFormEl.onsubmit = function (e) {
-    e.preventDefault()
+ColorPicker.fixIndicators(sliderEl, pickerEl);
+
+var cp = new ColorPicker(
+                document.getElementById('slider'), 
+                document.getElementById('picker'), 
+                function(hex, hsv, rgb, value, pickerCoordinate, sliderCoordinate) {
+                    ColorPicker.positionIndicators(sliderEl, pickerEl, sliderCoordinate, pickerCoordinate)
+                    whatEl.value = hex
+                    out(value)
+            });
+
+whatEl.onchange = function () {
+    var value = util.parseColor(whatEl.value)
+    if (value !== undefined) {
+        cp.setHex(util.hexColorify(value))
+        out(value)
+    }
+}
+
+function out(value) {
     var colorlist = colorlistEl.value
-    var searchColor = +whatEl.value
-    var closest = util.closestColor(colorlists[colorlist], searchColor)
-    outEl.innerHTML = formatOut("", searchColor) + formatOut(closest.name + " = ", closest.color)
+    var closest = util.closestColor(colorlists[colorlist], value)
+    outEl.innerHTML = formatOut("", value) + formatOut(closest.name + " = ", closest.color)
 }
 
 function formatOut(text, color) {
-    return '<div style="background-color:' + util.hexColorify(color) + ';color:' + (util.isDark(color) ? 'black' : 'white' ) + '"><p>' +
-                text + util.hexify(color) + '</p></div>'
+    return '<div style="background-color:' + util.hexColorify(color) + ';color:' + (util.isDark(color) ? 'white' : 'black' ) + '"><p>' +
+                text + util.hexColorify(color) + '</p></div>'
 }
+
+cp.setHex("#800000")
+
--- src/util.js
+++ src/util.js
@@ -1,3 +1,7 @@
+var colors = require('./official').concat(require('./xkcd')).map(function (v) {
+    return [v[0].toLowerCase(), v[1]]
+})
+
 
 function colorDistance(c1, c2) {
     var sum = 0;
@@ -30,25 +34,64 @@ function closestColor(list, c) {
     return best
 }
 
+function rgb2Value(r, g, b) {
+    return b | (g << 8) | (r << 16)
+}
+
+function parseColor(str) {
+    if (isNumber(str)) { // Probably hex literal
+        return +str
+    }
+    str = ("" + str).trim().toLowerCase()
+    if (str[0] == '#') { // hex
+        if (str.length == 7) { // hex6
+            return parseInt(str.substr(1), 16)
+        } else if (str.length == 4) { // hex3
+            return rgb2Value(
+                parseInt(str[1] + str[1], 16),
+                parseInt(str[2] + str[1], 16),
+                parseInt(str[3] + str[1], 16))
+        }
+    }
+    if (str.indexOf('rgb') === 0) { //rgba(a)
+        var match = str.match(/rgba?\(([^)]+)\)/)[1]
+        var parts = match && match.split(/ *, */).map(Number);
+        if (parts && (parts.length === 3 || parts.length === 4)) {
+            return rgb2Value(parts[0], parts[1], parts[2])
+        }
+    }
+
+    for (var i = 0; i < colors.length; i++) {
+        if (colors[i][0] == str) {
+            return colors[i][1];
+        }
+    }
+}
+
+
+function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
 function hexify(c) {
     return "0x" + (+c).toString(16).toUpperCase()
 }
 
 function hexColorify(c) {
-    return "#" + (+c).toString(16).toUpperCase()
+    return "#" + (16777216 | c).toString(16).slice(1).toUpperCase()
 }
 
 function luma(c) {
     var rgb = +c;
     var r = (rgb >> 16) & 0xff;
-    var g = (rgb >>  8) & 0xff;
-    var b = (rgb >>  0) & 0xff;
+    var g = (rgb >> 8) & 0xff;
+    var b = (rgb >> 0) & 0xff;
 
-    return  0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+    return 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
 }
 
 function isDark(c) {
-    return luma(c) < 40;
+    return luma(c) < 100;
 }
 
 
@@ -58,3 +101,6 @@ module.exports.hexify = hexify
 module.exports.hexColorify = hexColorify
 module.exports.luma = luma
 module.exports.isDark = isDark
+module.exports.isNumber = isNumber
+module.exports.parseColor = parseColor
+module.exports.rgb2Value= rgb2Value
